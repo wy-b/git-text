46dc2f7c9bff000df57cd0bde63990ebe2189310
wy-b <147574645+wy-b@users.noreply.github.com>
2023-10-12 10:48:27 +0800

    pull commit

--- src/main/java/GitText.java
+++ src/main/java/GitText.java
@@ -6,5 +6,7 @@ public class GitText {
         System.out.println("你好，世界!!!");
         System.out.println("你好，java!!!");
         System.out.println("一万年以后");
+        System.out.println("pull 拉取代码");
+        
     }
 }
